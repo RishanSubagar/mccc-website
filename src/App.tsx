08a556ac98eb4a0c07a4ee12c16faa9bccebb3c4
RishanSubagar <rishans48@gmail.com>
2025-04-06 23:01:40 -0400

    create slideshow

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,21 @@ import MenuIcon from "@mui/icons-material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Toolbar from "@mui/material/Toolbar";
 import React from "react";
+import Carousel from "./components/Carousel.tsx"; 
+import { EmblaOptionsType } from 'embla-carousel'
+
+import {
+  APIProvider
+} from "@vis.gl/react-google-maps";
 
 
 const sections = ["About", "Contact"];
 
 function App() {
+  const OPTIONS: EmblaOptionsType = {}
+  const SLIDE_COUNT = 5
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -39,7 +49,7 @@ function App() {
                   display: { xs: "none", md: "flex" },
                   fontFamily: "monospace",
                   fontWeight: 700,
-                  letterSpacing: ".1rem",
+                  letterSpacing: ".04rem",
                   color: "black",
                   textDecoration: "none",
                 }}
@@ -114,6 +124,23 @@ function App() {
           </Toolbar>
         </Container>
       </AppBar>
+      <Container>
+        <section id="display">
+          <Typography variant="h4" sx={{ mt: 4 }}>
+            Precision in Every Detail
+          </Typography>
+          <Carousel slides={SLIDES} options={OPTIONS} />
+        </section>
+        <section id="about">
+          <Typography variant="h4" sx={{ mt: 4 }}>
+            Dedicated to Precision, Driven by Excellence
+          </Typography>
+          <p>We are driven by precision, craftsmanship, and a relentless pursuit of excellence. In our work, attention to detail is non-negotiable, and continuous improvement is a way of life. We believe in earning trust through consistency, delivering high-quality results that exceed expectations. By collaborating closely with clients, suppliers, and team members, we ensure that every project contributes to shared success and long-term partnerships built on reliability and respect.</p>
+        </section>
+        {/* <APIProvider>
+          <section id="contact"></section>
+        </APIProvider> */}
+      </Container>
     </div>
   );
 }
